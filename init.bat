8b08cf1152832069faa8b3cc1c45054f31431aa2
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-16 04:26:16 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# python-learning" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/python-learning.git
-git push -u origin main
